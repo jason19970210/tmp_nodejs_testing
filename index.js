e1ff7a1ff7bc12ee918935be867ed61cb56589b3
jason19970210 <jason19970210@gmail.com>
2021-07-13 10:48:17 +0800

    basic implementation

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Start")
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    //res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', (socket) => {
+    console.log('front-end connected');
+})
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
