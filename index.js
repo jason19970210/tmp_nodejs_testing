10cb201b5fddecd7d763dc2ef9920617919240c6
jason19970210 <jason19970210@gmail.com>
2021-07-15 01:30:01 +0800

    seperate mqtt & socket

--- index.js
+++ index.js
@@ -9,25 +9,30 @@ const io = new Server(server);
 
 const mqtt_client = mqtt.connect('mqtt://120.126.16.88', opt);
 
+
 mqtt_client.on('connect', function(){
     console.log('server connected to broker');
     mqtt_client.subscribe('test');
 })
 
+mqtt_client.on('message', (_, msg) => {
+    // store msg to redis database
+    io.sockets.emit('test', msg.toString()); // to all socket clients
+})
+
 app.get('/', (req, res) => {
-    //res.send('<h1>Hello world</h1>');
     res.sendFile(__dirname + '/index.html');
 });
 
 
-io.on('connection', (socket) => {
-    console.log('front-end connected');
-    //socket.emit('test', "123") // correct
-    mqtt_client.on('message', function(topic, msg){
-        console.log("Topic: ", topic, ", msg: ", msg.toString());
-        socket.emit('test', msg.toString());
-    })
-})
+// io.on('connection', (socket) => {
+//     console.log('front-end connected');
+//     //socket.emit('test', "123") // correct
+//     // mqtt_client.on('message', function(topic, msg){
+//     //     console.log("Topic: ", topic, ", msg: ", msg.toString());
+//     //     socket.emit('test', msg.toString());
+//     // })
+// })
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
