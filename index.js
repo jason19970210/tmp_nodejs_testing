fc7a3a8d62640e7ca2f38d8bf1e3b9835f3f5d40
jason19970210 <jason19970210@gmail.com>
2021-07-21 21:19:10 +0800

    Update HTML with append elements innerHTML

--- index.js
+++ index.js
@@ -10,13 +10,48 @@ const io = new Server(server);
 const mqtt_client = mqtt.connect('mqtt://120.126.16.88', opt);
 
 
+// Functions
+function getDatetime(){
+    let date_ob = new Date();
+
+    // adjust 0 before single digit date
+    let date = ("0" + date_ob.getDate()).slice(-2);
+
+    // current month
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+    // current year
+    let year = date_ob.getFullYear();
+
+    // current hours
+    let hours = date_ob.getHours();
+
+    // current minutes
+    let minutes = date_ob.getMinutes();
+
+    // current seconds
+    let seconds = date_ob.getSeconds();
+
+    // prints date in YYYY-MM-DD format
+    //console.log(year + "-" + month + "-" + date);
+
+    // prints date & time in YYYY-MM-DD HH:MM:SS format
+    // console.log(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
+    let result = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds
+    
+    // prints time in HH:MM format
+    //console.log(hours + ":" + minutes);
+
+    return result
+}
+
 mqtt_client.on('connect', function(){
     console.log('server connected to broker');
     mqtt_client.subscribe('test');
 })
 
 mqtt_client.on('message', (_, msg) => {
-    console.log(msg.toString());
+    console.log( getDatetime() + " > "+ msg.toString());
     // store msg to redis database
     io.sockets.emit('test', msg.toString()); // to all socket clients
 })
